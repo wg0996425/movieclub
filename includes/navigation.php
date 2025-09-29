857387176c641a17f3f326e5c5dd4ab032746f4e
Viktor Gray <vg0996425@otc.edu>
2025-09-29 08:13:09 -0500

    changed something?

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,6 +1,7 @@
 <nav class="navbar bg-light mb-3">
     <div class="container">
         <a class="navbar-brand" href="index.php">Home</a>
+        <a class="nav-link" href="welcome.php?member=true">Welcome</a>
         <a class="nav-link" href="welcome.php?member=false">VIP Area</a>
     </div>
 </nav>
